8f0715f8acf6ceb4e8a7ced7fd33196e20e2dd3e
sainadh2845 <22jr1a4248@gmail.com>
2024-06-10 19:32:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
 
-# Function to get search results from Wikipedia
 def get_wikipedia_search_results(search_term):
     endpoint = "https://en.wikipedia.org/w/api.php"
     params = {
@@ -13,28 +12,22 @@ def get_wikipedia_search_results(search_term):
     response = requests.get(endpoint, params=params)
     return response.json()['query']['search'][:3]
 
-# Streamlit app layout
 st.title('Wikipedia Search App')
 
-# Search input
-search_term = st.text_input('Enter a search term:', '')
 
-# Search button
-if st.button('Search'):
+search_term = st.text_input('Enter a search element:', '')
+if st.button('Enter'):
     if search_term:
-        # Get the top 3 search results from Wikipedia
+
         results = get_wikipedia_search_results(search_term)
 
-        # Display the results
         for result in results:
             title = result['title']
             snippet = result['snippet']
             url = f"https://en.wikipedia.org/?curid={result['pageid']}"
 
-            # Display each result as a clickable link with a snippet
+    
             st.markdown(f"### {title}")
             st.markdown(snippet)
     else:
-        st.error('Please enter a search term.')
-
-# Run the Streamlit app by saving this script and running `streamlit run your_script.py`
+        st.error('Please enter a search element.')
